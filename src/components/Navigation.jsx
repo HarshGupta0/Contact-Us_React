4d030fdc55890353db96a1fe9913379695ff5b51
harshgupta0 <hg09032@gmail.com>
2024-07-17 20:26:48 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+  return (
+    <nav>
+        <div className="logo">
+            <img src="./images/Frame.png" alt="" />
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      
+    </nav>
+  )
+}
+
+export default Navigation;
